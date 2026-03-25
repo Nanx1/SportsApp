d49da0d5ee300f80e6610d97243d5bc076c8b3f1
Nanx1 <514748617@qq.com>
2026-03-25 14:28:17 +0800

    好好好

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,10 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
 }
 
 android {
     namespace = "com.example.sports"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.sports"
@@ -33,9 +28,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
 }
 
